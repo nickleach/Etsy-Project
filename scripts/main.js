54772b3d753bc6f736cc34495ceb88315dada7cb
Nick Leach <nickleach22@gmail.com>
2015-05-24 16:35:06 -0400

    Added Hovers

--- scripts/main.js
+++ scripts/main.js
@@ -19,16 +19,20 @@ $(document).ready( function(){
         itemImage,
         itemSeller,
         itemPrice,
+        pic1,
+        pic2,
         block;
 
 
     guitarApi.results.forEach( function(item){
-      itemImage = '<img src="'+item.Images[0].url_170x135 +'">';
+      itemImage = '<img class= itemImage src="'+item.Images[0].url_170x135 +'">';
       itemTitle = '<div class= "row"><a href= "#" class="item-title">' + item.title + '</a></div>';
-      itemSeller = '<div clas= "row"><a href= "#" class="item-seller">' + item.Shop.shop_name + '</a></div>';
+      itemSeller = '<div class= "row"><a href= "#" class="item-seller">' + item.Shop.shop_name + '</a></div>';
       itemPrice = '<p class="item-price">' + '$' + item.price + ' ' + item.currency_code +'</p>';
+      pic1= '<span class = "pic1"><img src ="images/hamburger.png"></span>';
+      pic2= '<span class = "pic2"><img src ="images/heart.png"></span>';
 
-      block = '<li class= "main-li">' + itemImage + itemTitle + itemSeller + itemPrice +'</li>';
+      block = '<li class= "main-li">' + itemImage + itemTitle + itemSeller + itemPrice + pic1 + pic2 +'</li>';
 
       $('.main-ul').append(block);
     });
